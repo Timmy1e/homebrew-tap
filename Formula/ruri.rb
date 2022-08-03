48932be880477c3e6e3a4ab33ccf6b24764ea939
Tim van Leuverden <TvanLeuverden@Gmail.com>
2022-08-03 20:39:52 +0200

    Ruri 2.0.1

--- Formula/ruri.rb
+++ Formula/ruri.rb
@@ -1,14 +1,18 @@
 class Ruri < Formula
-  desc "Program that calculates the CRC-32 and checks it against the filename"
+  desc "Calculates the CRC-32 of files and checks them against their filename"
   homepage "https://gitlab.com/Timmy1e/ruri/"
-  url "https://gitlab.com/Timmy1e/ruri/-/archive/v2.0.0/ruri-v2.0.0.tar.gz"
-  sha256 "65638f066d5f1f319b4a3ba82ed214a31dbdd8a5ab34d3bbff9b85df38c67b2e"
+  url "https://gitlab.com/Timmy1e/ruri/-/archive/v2.0.1/ruri-v2.0.1.tar.gz"
+  sha256 "ef7883661b0f145b6bf9b7d6be172b2c17198003625f28ce7950065d489f5a20"
   license "AGPL-3.0-or-later"
 
   bottle do
     root_url "https://ghcr.io/v2/timmy1e/tap"
     sha256 cellar: :any_skip_relocation, big_sur:      "e08280cc2473a4408e803d3d71260b5fc5fe468feb55cd640608e7fdba0fa238"
     sha256 cellar: :any_skip_relocation, x86_64_linux: "d3c7bee6d817275872117c43b3b8e7be2f09f0026bcc59cccf1b6f379d3861d9"
+  end
+
+  head do
+    url "https://gitlab.com/Timmy1e/ruri.git", branch: "master"
   end
 
   depends_on "rust" => :build
