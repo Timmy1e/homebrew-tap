5db9adc03c5952a8219c4598f5abd0a837101894
Tim van Leuverden <TvanLeuverden@Gmail.com>
2025-10-22 15:27:39 +0200

    ruri: add 2.1.1 bottle.

--- Formula/ruri.rb
+++ Formula/ruri.rb
@@ -5,6 +5,13 @@ class Ruri < Formula
   sha256 "5a2341444a6165360035f1b6e04d782f2bcc79fc46d13502166108093691a3b1"
   license "AGPL-3.0-or-later"
 
+  bottle do
+    root_url "https://ghcr.io/v2/timmy1e/tap"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "36ad832de92a83d0ced4af720167d5f74f4327165d19c63e782871b46c40ea9f"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "96a56bed2205af2d82a53900225b8a3d44397fef7b1c1b66c151c2332ac358e5"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "ab6addacc2401e5a1b25b663221f3385be7d2b15d9eb63aed45d57a1615186b2"
+  end
+
   depends_on "rust" => :build
 
   def install
